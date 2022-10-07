0ed7a0ca44210c7b206128200e5ac5cebe6aec3b
Miguel Quesada <miguel.quesada.martinez.1975@gmail.com>
2022-10-07 18:33:01 +0200

    Allow console writing

--- src/main/java/com/ironhack/Homework3/Homework3Application.java
+++ src/main/java/com/ironhack/Homework3/Homework3Application.java
@@ -1,14 +1,27 @@
 package com.ironhack.Homework3;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Homework3Application {
+public class Homework3Application  implements CommandLineRunner {
 
 	public static void main(String[] args) {
 
 		SpringApplication.run(Homework3Application.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("\n\n");
+		System.out.println("#################");
+		System.out.println("Escoje opción:");
+		System.out.println("1. Crear Lead:");
+		System.out.println("2. Mostrar Leads");
+		System.out.println("3. Convertir Lead");
+		System.out.println("4. Cancelar Oportunidad:");
+		System.out.println("5. Salir:");
+		System.out.println("O escribe un comando:");
+	}
 }
