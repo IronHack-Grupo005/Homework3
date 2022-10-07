8917451c541eefaf991317671cced97d871805f0
Miguel Quesada <miguel.quesada.martinez.1975@gmail.com>
2022-10-07 19:07:18 +0200

    Repositories

--- src/main/java/com/ironhack/Homework3/repositories/OpportinityRepository.java
+++ src/main/java/com/ironhack/Homework3/repositories/OpportinityRepository.java
@@ -1,10 +1,10 @@
 package com.ironhack.Homework3.repositories;
 
-import com.ironhack.Homework3.classes.Contact;
+import com.ironhack.Homework3.classes.Opportunity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ContactRepository extends JpaRepository<Contact, Long> {
+public interface OpportinityRepository extends JpaRepository<Opportunity, Long> {
 
 }
