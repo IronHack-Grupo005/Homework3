a3a03d48d1401d87e8597de550f39b2eafef113c
Miguel Quesada <miguel.quesada.martinez.1975@gmail.com>
2022-10-08 21:46:37 +0200

    Addng select * from leads

--- src/main/java/com/ironhack/Homework3/repositories/LeadRepository.java
+++ src/main/java/com/ironhack/Homework3/repositories/LeadRepository.java
@@ -1,14 +1,21 @@
 package com.ironhack.Homework3.repositories;
 
 import com.ironhack.Homework3.classes.Leads;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface LeadRepository extends JpaRepository<Leads, Long> {
 
     Optional<Leads>findByName(String name);
+
+    @Query(value = "Select * from lead", nativeQuery = true)
+    List<Leads> findAll();
+
 
 }
